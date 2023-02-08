b8e8de1a1e2aedd2111ec98cf8544b95e6011b29
Yue Dong <ydong011@fiu.edu>
2023-02-08 18:59:48 +0800

    update

---
+++ todolist/src/app/todolist/todolist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.scss']
+})
+export class TodolistComponent {
+
+}
