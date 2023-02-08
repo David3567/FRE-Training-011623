7d4b4292fcebfb3f56481c2558abc1e63cbe97a6
Yue Dong <ydong011@fiu.edu>
2023-02-08 16:07:13 +0800

    update

--- JS/index.js
+++ JS/index.js
@@ -24,16 +24,60 @@
 
 const obj = {
   name: 'YY',
-  foo: function() {
-    // arrow function.
-    (function (self) {
-      console.log(self)
-    }).bind()
+  foo: function(baz) {
+    // this;
+    // const baz = () => {
+    //   console.log(this);
+    // };
+    baz();
+
+    // const bar = (function (...args) {
+    //   console.log('arguments: ', arguments);
+      
+    //   console.log(this);
+    // }).bind(this);
+    // bar(1, 2, 3, true, 'hello');
   }
 }
-obj.foo();
+// obj.foo();
 
-// bind, call, apply
+// // bind, call, apply
+
+// function foo(num1, boolean1) { // 2
+//   console.log(num1, boolean1, this.name);
+// }
+// // // const newfoo = foo.bind(obj);
+// // // newfoo(2, false)
+
+// in a class 
+class Person {
+
+  constructor(name, age) {
+    console.log(this);
+  }
+
+  foo() {
+    console.log(this);
+  }
+}
+const p = new Person('David', 67);
+
+class MyPromise {
+  thenQueue
+
+  constructor(executor) {
+    executor(this.resolve.bind(this), this.reject);
+  }
+  
+  resolve() {
+    console.log(this);
+  }
+  reject = () => {}
+}
+
+// foo.call(obj, 445, true) // 1 + 2 = 3
+// foo.apply(obj, [445, true]) // tarobj + 1: arr, arr.length ===> 100
+
 
 
 // promise
