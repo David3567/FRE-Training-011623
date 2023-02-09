6afcd9ac091c318b224dd291aaaba385cc273b94
Yue Dong <ydong011@fiu.edu>
2023-02-09 18:02:45 +0800

    update

--- TS/typescript/index.ts
+++ TS/typescript/index.ts
@@ -1,21 +1,71 @@
-function course(name: string) {
-	return function (target: Function) {
-		target.prototype.name = name;
-	};
+// function component(target: Function) {
+// 	target.prototype.name = "David";
+// }
+
+// function component(name: string) {
+// 	return function (target: Function) {
+// 		target.prototype.name = name;
+// 	};
+// }
+
+// @component({
+// })
+// class Person {}
+
+// const p = new Person();
+// console.log(p);
+
+//interface, class, union, generic, enum
+
+// let hello: "TT" | "HH" = "TT";
+
+// // type fnType = (name: string, age: number) => [string, number];
+// interface fnType {
+// 	(name: string, age: number): [string, number];
+// }
+
+// const foo: fnType = function () {
+// 	return ["hello", 34];
+// };
+
+// foo("hi", 56);
+
+// generic
+
+// function toStringArr(x: string, y: string): string[] {
+// 	return [x, y];
+// }
+// function toNumbeArr(x: number, y: number): number[] {
+// 	return [x, y];
+// }
+// function toArr<T, R>(x: T, y: R): [T, R] {
+// 	return [x, y];
+// }
+// toArr<number, string>(12, "45");
+
+// interface Queue<T> {
+//     enqueue(item: T): void;
+//     dequeue(): T;
+//     getqueue(): T[];
+// }
+// class MyQueue<T> implements Queue<T> {
+//     queue: T[];
+
+//     enqueue(item: T): void {}
+//     dequeue(): T {
+//         throw new Error("Method not implemented.");
+//     }
+//     getqueue(): T[] {
+//         throw new Error("Method not implemented.");
+//     }
+// }
+
+enum Role {
+	USER = "user",
+	SUPER = 4,
+	ADMIN,
 }
 
-@course("Jojo")
-class Person {
-	name: string;
+const p: Role = Role.ADMIN;
 
-	constructor() {
-		// this.name = name;
-	}
-
-	printcourse() {
-		console.log("Angular: ", this.name);
-	}
-}
-
-const p = new Person();
-p.printcourse();
+console.log(p);
