c1a9b6f534ac3cfc224d586e2938177e0f043015
DanielWu1 <Tony153265964@gmail.com>
2023-02-11 18:43:22 -0500

    first push

--- TeamCCProject/src/app/app.module.ts
+++ TeamCCProject/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
