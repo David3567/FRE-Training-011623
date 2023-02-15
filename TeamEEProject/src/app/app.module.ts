8c19af3c44125bb2b2e8e260786646a09194f726
Liping Deng <lipingd2@illinois.edu>
2023-02-14 23:28:08 -0800

    homepage v1 fix bug

--- TeamEEProject/src/app/app.module.ts
+++ TeamEEProject/src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageModule } from './homepage/homepage.module';
+import { HomepageComponent } from './homepage/homepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    SearchBarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule,
-    HomepageModule
     
   ],
   providers: [],
