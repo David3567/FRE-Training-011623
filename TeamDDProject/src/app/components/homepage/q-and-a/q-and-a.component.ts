b74e89f1db25c67e35e9b18960eee8ec63f7976b
Kuqki <yanzhang@kuqki.org>
2023-02-15 19:54:52 -0500

    homepage finished

--- TeamDDProject/src/app/components/homepage/q-and-a/q-and-a.component.ts
+++ TeamDDProject/src/app/components/homepage/q-and-a/q-and-a.component.ts
@@ -13,7 +13,7 @@ export class QAndAComponent {
     {
       id: '1',
       question: 'What is Netflix?',
-      answer:"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.<br><br>You can watch as much as you want, whenever you want all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!",
+      answer:"Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you want all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!",
     },
     {
       id: '2',
@@ -28,7 +28,7 @@ export class QAndAComponent {
     {
       id: '4',
       question: 'Where can I watch?',
-      answer:"Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.<br><br>You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
+      answer:"Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles. You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
     },
     {
       id: '5',
@@ -43,13 +43,13 @@ export class QAndAComponent {
     {
       id: '7',
       question: 'Is Netflix good for kids?',
-      answer:"The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.<br><br>Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
+      answer:"The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space. Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
     },
   ];
 
   activeQnAId: string = "";
 
   changeState(qna : Question) {
-    this.activeQnAId = qna.id;
+    this.activeQnAId = this.activeQnAId == qna.id ? "" : qna.id;
   }
 }
