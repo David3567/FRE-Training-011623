5c4d2adf0c153d12bcd70ab9c269fa22935f69aa
Olive Daly <olive-branch@posteo.me>
2023-02-17 13:02:21 -0500

    implement register component logic, child components, and basic styling

--- TeamEEProject/src/app/app.module.ts
+++ TeamEEProject/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { RegisterPage1Component } from './register-page1/register-page1.component';
+import { RegisterPage2Component } from './register-page2/register-page2.component';
+import { RegisterPage3Component } from './register-page3/register-page3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RegisterPage1Component,
+    RegisterPage2Component,
+    RegisterPage3Component
   ],
   providers: [],
   bootstrap: [AppComponent]
