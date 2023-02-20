53f30b5efbadcff6d9d9dae6cad3c190a1911872
Yue Dong <ydong011@fiu.edu>
2023-02-21 00:13:41 +0800

    update

--- Angular/Forms/src/app/app.module.ts
+++ Angular/Forms/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './components/template-form/template-form.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
