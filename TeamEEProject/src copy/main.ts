44ffa0f2ec7f2ca05fa37a1ff668b4485f966bf1
Yuxin Fang <yuxinfang@Yuxins-Air.attlocal.net>
2023-02-20 16:02:20 -0600

    add login page

---
+++ TeamEEProject/src copy/main.ts
@@ -0,0 +1,9 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
