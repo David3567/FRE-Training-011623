bc480f70112d996e65b36017bb2189524cb7e914
YuxinFang99 <122831673+YuxinFang99@users.noreply.github.com>
2023-02-21 16:10:18 -0600

    add login page

---
+++ TeamEEProject/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginInPageComponent } from './login-in-page/login-in-page.component';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [  
+    LoginInPageComponent,
+    AppComponent,],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
