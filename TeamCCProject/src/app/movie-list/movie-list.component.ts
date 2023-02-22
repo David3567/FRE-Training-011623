8e37825cb237393da6dc953f95c6f11441117aa2
ariasplendidly <102254554+ariasplendidly@users.noreply.github.com>
2023-02-21 17:15:29 -0800

    page4-modification

--- TeamCCProject/src/app/movie-list/movie-list.component.ts
+++ TeamCCProject/src/app/movie-list/movie-list.component.ts
@@ -7,33 +7,15 @@ import { MovieServiceService } from '../movie-service.service';
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent implements OnInit {
-  movieList: Movie[] = [];
-  rows: Movie[][] = [];
+export class MovieListComponent implements OnInit{
+  movieList : Movie[] = [];
 
-  constructor(private movieService: MovieServiceService) { }
+  constructor(private movieService :MovieServiceService){ }
 
-  ngOnInit(): void {
-    let numRows = 6;
-    let numCols = 5;
-    let currentIndex = 0;
-
-    for (let i = 0; i < numRows; i++) {
-      let row: Movie[] = [];
-      for (let j = 0; j < numCols; j++) {
-        if (currentIndex < this.movieList.length) {
-          row.push(this.movieList[currentIndex]);
-          currentIndex++;
-        } else {
-          break;
-        }
-      }
-      this.rows.push(row);
-
-      this.movieService.getBooks().subscribe();
-      this.movieService.MovieList$.subscribe((data) => {
-        this.movieList = data;
-      })
-    }
+  ngOnInit(): void{
+    this.movieService.getBooks().subscribe();
+    this.movieService.MovieList$.subscribe((data) =>{
+      this.movieList = data;
+    })
   }
 }
