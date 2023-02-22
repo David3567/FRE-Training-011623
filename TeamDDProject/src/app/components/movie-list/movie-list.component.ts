4277cfcb577e9313af90f10d62b34e42c17073fa
Queenie Liu <qingminliu1995@outlook.com>
2023-02-22 03:06:49 -0800

    movie-list

--- TeamDDProject/src/app/components/movie-list/movie-list.component.ts
+++ TeamDDProject/src/app/components/movie-list/movie-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import  { Movie } from '../../interface/movie-interface'
+import { MovieServiceService } from '../../services/movie-service.service'
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
-export class MovieListComponent {
+export class MovieListComponent implements OnInit{
+    movies : Movie[] = [];
+    constructor(private movieService :MovieServiceService){ }
 
+    ngOnInit(): void{
+      this.movieService.getMovies().subscribe();
+      this.movieService.MovieList$.subscribe((data) =>{
+        this.movies = data;
+      })
+  }
 }
---
+++ TeamDDProject/src/app/interface/movie-interface.ts
@@ -0,0 +1,18 @@
+export interface Movie {
+    id: number;
+    img: string;
+    title: string;
+    rate: number;
+    date: string;
+    description: string;
+    language: string;
+    popularity: number;
+    video: boolean;
+    adult: boolean;
+  }
+  
+  export interface ApiData {
+    dates: object;
+    page: number;
+    results: Array<object>;
+  }
