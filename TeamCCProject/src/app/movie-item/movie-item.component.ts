49f809f9d690602fe112927e4ceb4763ea1a0a54
ariasplendidly <102254554+ariasplendidly@users.noreply.github.com>
2023-02-21 17:03:07 -0800

    page4

--- TeamCCProject/src/app/movie-item/movie-item.component.ts
+++ TeamCCProject/src/app/movie-item/movie-item.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../movie-interface';
+import { MovieServiceService } from '../movie-service.service';
 
 @Component({
   selector: 'app-movie-item',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.scss']
 })
-export class MovieItemComponent {
+
+
+
+export class MovieItemComponent implements OnInit {
+  @Input() movie?: Movie = {} as Movie;
+
+  movies: Movie[] = [];
+
+  constructor(private MovieService: MovieServiceService) { }
+
+  // ngOnInit() {
+  //   this.getMovies();
+  // }
+
+  // getMovies(): void {
+  //   this.MovieService.getMovies()
+  //     .subscribe((data: Movie[]) => {
+  //       this.movies = data;
+  //     });
+
+  ngOnInit(): void {
+    this.MovieService.getBooks().subscribe();
+    this.MovieService.MovieList$.subscribe((data) => {
+      this.movies = data;
+    })
+  }
 
 }
+
