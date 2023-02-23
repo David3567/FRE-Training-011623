be543c667aefc2572cd468744816752786bca8e6
Liping Deng <lipingd2@illinois.edu>
2023-02-22 19:46:39 -0800

    Done movielist; wait for movie item.

---
+++ TeamEEProject/src/app/movielist/movielist.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Movie, MoviesService } from '../movies.service';
+
+@Component({
+  selector: 'app-movielist',
+  templateUrl: './movielist.component.html',
+  styleUrls: ['./movielist.component.css']
+})
+export class MovielistComponent {
+  movies!:Movie[];
+  ngOnInit():void{
+    this.movieService.movies$.subscribe((movies:Movie[]) => {
+      this.movies = movies;
+      console.log(this.movies)
+    })
+    this.movieService.discoverMovies();
+  }
+  constructor(private movieService:MoviesService) {
+
+  }
+}
