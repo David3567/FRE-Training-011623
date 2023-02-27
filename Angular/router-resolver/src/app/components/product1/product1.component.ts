46f93ed4a67d735be78a63a8c3536bd8f9e5f7e4
Yue Dong <ydong011@fiu.edu>
2023-02-27 14:10:18 +0800

    update

--- Angular/router-resolver/src/app/components/product1/product1.component.ts
+++ Angular/router-resolver/src/app/components/product1/product1.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/Product.class';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product1',
   templateUrl: './product1.component.html',
-  styleUrls: ['./product1.component.scss']
+  styleUrls: ['./product1.component.scss'],
 })
-export class Product1Component {
+export class Product1Component implements OnInit {
+  public products!: Product[];
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    console.log('ngOnInit');
+
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
