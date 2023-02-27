0d9a7a2caf84fe455d26b7ece3a6a760e89167ba
Yue Dong <ydong011@fiu.edu>
2023-02-27 14:12:34 +0800

    update

--- Angular/router-resolver/src/app/components/product2/product2.component.ts
+++ Angular/router-resolver/src/app/components/product2/product2.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/model/Product.class';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product2',
   templateUrl: './product2.component.html',
-  styleUrls: ['./product2.component.scss']
+  styleUrls: ['./product2.component.scss'],
 })
 export class Product2Component {
+  public products!: Product[];
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    this.products = this.route.snapshot.data['products'];
+  }
 }
