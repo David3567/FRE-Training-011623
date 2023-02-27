00592716dbea43fc0ff68a90da6e881e30bf894a
Yue Dong <ydong011@fiu.edu>
2023-02-27 13:52:23 +0800

    update

--- Angular/router-resolver/src/app/app-routing.module.ts
+++ Angular/router-resolver/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { Product1DetailComponent } from './components/product1-detail/product1-detail.component';
+import { Product1Component } from './components/product1/product1.component';
+import { Product2DetailComponent } from './components/product2-detail/product2-detail.component';
+import { Product2Component } from './components/product2/product2.component';
+import { ProductListResolver } from './services/product-list.resolver';
+import { ProductResolver } from './services/product.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'product1', component: Product1Component },
+  {
+    path: 'product2',
+    component: Product2Component,
+    resolve: { products: ProductListResolver },
+  },
+  { path: 'product1/:id', component: Product1DetailComponent },
+  {
+    path: 'product2/:id',
+    component: Product2DetailComponent,
+    resolve: { product: ProductResolver },
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
