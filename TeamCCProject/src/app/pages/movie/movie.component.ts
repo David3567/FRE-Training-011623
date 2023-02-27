42836b1a22eb9d797668f8007255234ad342bc85
DanielWu1 <Tony153265964@gmail.com>
2023-02-26 22:59:11 -0500

    Folder Structure changed

---
+++ TeamCCProject/src/app/pages/movie/movie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  templateUrl: './movie.component.html',
+  styleUrls: ['./movie.component.scss']
+})
+export class MovieComponent {
+
+}
