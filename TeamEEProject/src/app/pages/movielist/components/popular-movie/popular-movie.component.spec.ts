66bfc905e3f355ca6d3188f80e2c620fb40086c1
Liping Deng <lipingd2@illinois.edu>
2023-03-04 21:56:35 -0800

    spec

--- TeamEEProject/src/app/pages/movielist/components/popular-movie/popular-movie.component.spec.ts
+++ TeamEEProject/src/app/pages/movielist/components/popular-movie/popular-movie.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PopularMovieListComponent } from './popular-movie-list.component';
+import { PopularMovieComponent } from './popular-movie.component';
 
 describe('PopularMovieListComponent', () => {
-  let component: PopularMovieListComponent;
-  let fixture: ComponentFixture<PopularMovieListComponent>;
+  let component: PopularMovieComponent;
+  let fixture: ComponentFixture<PopularMovieComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PopularMovieListComponent ]
+      declarations: [ PopularMovieComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PopularMovieListComponent);
+    fixture = TestBed.createComponent(PopularMovieComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
