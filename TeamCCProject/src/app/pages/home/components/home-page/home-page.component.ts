bb654e608013ee773a19211242ffd54cc1164141
DanielWu1 <Tony153265964@gmail.com>
2023-03-09 00:58:05 -0500

    add router link

--- TeamCCProject/src/app/pages/home/components/home-page/home-page.component.ts
+++ TeamCCProject/src/app/pages/home/components/home-page/home-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthServiceService } from 'src/app/service/authService/auth-service.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  emailInput!: FormGroup;
 
+  constructor(private fb:FormBuilder, private router: Router, private userServer: AuthServiceService){}
+
+  ngOnInit(){
+    this.emailInput = this.fb.group({
+      email: ['']
+    })
+  }
+  onNext(){
+    // console.log(this.emailInput.value)
+    this.userServer.addNewUser(this.emailInput.value);
+    this.router.navigate(['/register/step1'])
+  }
 }
